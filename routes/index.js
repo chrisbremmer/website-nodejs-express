061e309c438ead7a47d571a4c63d8b40ac160aa0
chrisbremmer <c_bremmer@outlook.com>
2020-10-05 09:59:51 -0700

    created routes for all subpages

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 
+const speakersRoute = require('./speakers')
+const feedbackRoute = require('./feedback')
+
 const router = express.Router()
 
 module.exports = () => {
@@ -8,5 +11,9 @@ module.exports = () => {
       pageTitle: 'Welcome'
     })
   })
+
+  router.use('/speakers', speakersRoute())
+  router.use('/feedback', feedbackRoute())
+
   return router
 }
